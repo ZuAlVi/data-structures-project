53f868d231901e68c12f45a9f501cd422a712d38
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-16 11:59:17 +0300

    Created test_stack_pop in test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -4,14 +4,24 @@ import unittest
 from src.stack import Stack
 
 stack = Stack()
+stack_2 = Stack()
 
 
 class TestStack(unittest.TestCase):
 
-    def test_Stack_Push(self):
+    def test_stack_push(self):
         stack.push('data1')
         stack.push('data2')
         self.assertEquals(stack.top.data, 'data2')
         self.assertEquals(stack.top.next_node.data, 'data1')
         with self.assertRaises(AttributeError):
             stack.top.next_node.next_node.data
+
+    def test_stack_pop(self):
+        stack_2.push('data1')
+        self.assertEquals(stack_2.pop(), 'data1')
+        self.assertIsNone(stack_2.top)
+        stack_2.push('data1')
+        stack_2.push('data2')
+        self.assertEquals(stack_2.pop(), 'data2')
+        self.assertEquals(stack_2.top.data, 'data1')
