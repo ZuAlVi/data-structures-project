b521b6917ede9d8db830b721f985b157c48b699f
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-19 12:38:41 +0300

    Added __str__ and enqueue method body to class Queue and __init__ method body to class Node in queue.py

--- src/queue.py
+++ src/queue.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,7 +25,11 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        temp = Node(data)
+        if self.tail is None:
+            self.head = self.tail = temp
+        self.tail.next_node = temp
+        self.tail = temp
 
     def dequeue(self):
         """
@@ -35,4 +41,7 @@ class Queue:
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        if self.head is None:
+            return ""
+        return f'{self.head.data}\n{self.head.next_node.data}\n{self.tail.data}'
+
