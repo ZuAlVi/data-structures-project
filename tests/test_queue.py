6326e29aa0796e6a8a41868f4f5b8a6a0be60563
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-19 12:32:53 +0300

    Added test_queue_enqueue function in test_queue.py

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+
+from src.queue import Queue
+
+queue = Queue()
+
+
+class TestQueue(unittest.TestCase):
+
+    def test_queue_enqueue(self):
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        self.assertEquals(queue.head.data, 'data1')
+        self.assertEquals(queue.head.next_node.data, 'data2')
+        self.assertEquals(queue.tail.data, 'data3')
+        with self.assertRaises(AttributeError):
+            queue.tail.next_node.data
