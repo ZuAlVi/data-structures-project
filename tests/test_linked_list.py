45b9ab477ebac7c1199ea53228ad80dcc48a54a8
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-27 18:47:54 +0300

    Added tests in test_linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,29 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+
+from src.linked_list import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_linked_list_insert_beginning(self):
+        ll = LinkedList()
+        ll.insert_beginning({'data1': 1})
+        ll.insert_beginning({'data2': 2})
+        self.assertEquals(ll.head.data, {'data2': 2})
+        self.assertEquals(ll.head.next_node.data, {'data1': 1})
+
+    def test_linked_list_insert_at_end(self):
+        ll = LinkedList()
+        ll.insert_beginning({'data1': 1})
+        ll.insert_at_end({'data2': 2})
+        self.assertEquals(ll.tail.data, {'data2': 2})
+        self.assertEquals(ll.tail.next_node, None)
+
+    def test_linked_list_str(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+        self.assertEquals(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
