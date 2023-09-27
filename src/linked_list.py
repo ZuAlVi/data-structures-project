c5f1974372bb3e6545212b6a5067223d612468ac
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-27 18:49:12 +0300

    Added method bodies in linked_list.py

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,32 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data)
+        if self.head is None:
+            new_node.next_node = self.head
+            self.head = self.tail = new_node
+        new_node.next_node = self.head
+        self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+        self.tail.next_node = new_node
+        self.tail = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -26,4 +40,4 @@ class LinkedList:
             node = node.next_node
 
         ll_string += ' None'
-        return ll_string
+        return ll_string.strip()
