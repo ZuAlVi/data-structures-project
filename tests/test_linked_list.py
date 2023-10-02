98058885c182f53b28181430127cd92b262c4f25
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-02 18:30:41 +0300

    Added test_linked_list_to_list function in test_linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -27,3 +27,16 @@ class TestLinkedList(unittest.TestCase):
         ll.insert_at_end({'id': 3})
         ll.insert_beginning({'id': 0})
         self.assertEquals(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_linked_list_to_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end({'id': 2, 'username': 'mik.roz'})
+        self.assertEquals(ll.to_list(), [
+            {'id': 1, 'username': 'lazzy508509'},
+            {'id': 2, 'username': 'mik.roz'},
+        ])
+
+
+
+
